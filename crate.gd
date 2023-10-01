d6c61bdfcaf2d7d1f0194e0c704264e6c08a3bf1
Huntracony <Huntracony@users.noreply.github.com>
2023-10-01 12:22:43 +0200

    Lowered gravity when grabbed

--- crate.gd
+++ crate.gd
@@ -1,10 +1,12 @@
 extends RigidBody2D
 
 const FORCE_FACTOR = 15
+const NORMAL_GRAVITY = 1.0
+const GRABBED_GRAVITY = 0.5
 func _ready():
 	linear_damp = 1.0
 	angular_damp = 0.1
-	gravity_scale = 1.0
+	gravity_scale = NORMAL_GRAVITY
 
 var grabbed = false
 var grabPos = Vector2()
@@ -20,10 +22,12 @@ func _input(event):
 	if not event is InputEventMouseButton: return
 	if event.pressed or event.button_index != 1: return
 	grabbed = false
+	gravity_scale = NORMAL_GRAVITY
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if not event is InputEventMouseButton: return
 	if not event.pressed or event.button_index != 1: return
 	print(event)
 	grabbed = true
+	gravity_scale = GRABBED_GRAVITY
 	grabPos = get_local_mouse_position()
