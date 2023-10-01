d485e779a8ac2fd19f1a309e2efe1ef75efb8495
Huntracony <Huntracony@users.noreply.github.com>
2023-10-01 11:15:59 +0200

    Renamed container to crate

--- root.gd
+++ root.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-var containerScene = preload("res://container.tscn")
+var crateScene = preload("res://crate.tscn")
 
 func _input(event):
 	if not event is InputEventMouseButton: return
 	if not event.pressed or event.button_index != 2: return
-	var container = containerScene.instantiate()
-	container.position = %ContainerContainer.get_local_mouse_position()
-	%ContainerContainer.add_child(container)
+	var crate = crateScene.instantiate()
+	crate.position = %CrateContainer.get_local_mouse_position()
+	%CrateContainer.add_child(crate)
