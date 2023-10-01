0a09fba5b55f9b344b46491f670e9c0743771d19
Leyla Perotti <leyrotti@gmail.com>
2023-10-01 13:00:27 +0200

    Max distance speed implemented

--- crate.gd
+++ crate.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 const FORCE_FACTOR = 15
 const NORMAL_GRAVITY = 1.0
 const GRABBED_GRAVITY = 0.5
+const MAX_DISTANCE = 200
 func _ready():
 	linear_damp = 1.0
 	angular_damp = 0.1
@@ -15,7 +16,11 @@ func _physics_process(_delta):
 	if not grabbed: return
 	var globalGrabPos = to_global(grabPos)
 	var force = get_global_mouse_position() - globalGrabPos
+	var distance_length = force.length()
+	if distance_length > MAX_DISTANCE:
+		force = force.normalized() * MAX_DISTANCE
 	var offset = globalGrabPos - global_position
+#	print(force * FORCE_FACTOR)
 	apply_force(force * FORCE_FACTOR, offset)
 
 func _input(event):
