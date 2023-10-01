2d4664e2adc06746c689bfa38d6150fda079b50b
Huntracony <Huntracony@users.noreply.github.com>
2023-10-01 12:19:40 +0200

    added crates

--- root.gd
+++ root.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
-var crateScene = preload("res://crate.tscn")
+var crateScene = preload("res://crates/cat-crate.tscn")
+var crateScenes = [
+	preload("res://crates/cat-crate.tscn"),
+	preload("res://crates/fox1-crate.tscn"),
+	preload("res://crates/fox2-crate.tscn"),
+	preload("res://crates/frog-crate.tscn"),
+	preload("res://crates/koala-crate.tscn"),
+	preload("res://crates/lion-crate.tscn"),
+	preload("res://crates/panda-crate.tscn"),
+	preload("res://crates/reindeer-crate.tscn"),
+	preload("res://crates/sera-crate.tscn"),
+	preload("res://crates/sloth-crate.tscn")
+]
 
 func _input(event):
 	if not event is InputEventMouseButton: return
 	if not event.pressed or event.button_index != 2: return
-	var crate = crateScene.instantiate()
+	var packedScene = crateScenes[ randi() % crateScenes.size() ]
+	var crate = packedScene.instantiate()
 	crate.position = %CrateContainer.get_local_mouse_position()
 	%CrateContainer.add_child(crate)
