a2cb11b1db0b3cfdd3e3608f42a14b166174fd0e
Leyla Perotti <leyrotti@gmail.com>
2023-10-02 21:25:08 +0200

    Oh gods everything broke

--- CrateTypeImage.gd
+++ CrateTypeImage.gd
@@ -1,19 +1,17 @@
-extends Sprite2D
+extends TextureRect
 
-#var crateScenes = [
-#	preload("res://crates/cat-crate.tscn"),
-#	preload("res://crates/fox1-crate.tscn"),
-#	preload("res://crates/fox2-crate.tscn"),
-#	preload("res://crates/frog-crate.tscn"),
-#	preload("res://crates/koala-crate.tscn"),
-#	preload("res://crates/lion-crate.tscn"),
-#	preload("res://crates/panda-crate.tscn"),
-#	preload("res://crates/reindeer-crate.tscn"),
-#	preload("res://crates/sera-crate.tscn"),
-#	preload("res://crates/sloth-crate.tscn")
-#]
-
-
+var crateScenes = {
+	fox = preload("res://crates/fox-crate.tscn"),
+	frog = preload("res://crates/frog-crate.tscn"),
+	koala = preload("res://crates/koala-crate.tscn"),
+	lion = preload("res://crates/lion-crate.tscn"),
+	panda = preload("res://crates/panda-crate.tscn"),
+	"red panda" = preload("res://crates/red-panda-crate.tscn"),
+	reindeer = preload("res://crates/reindeer-crate.tscn"),
+	sera = preload("res://crates/sera-crate.tscn"),
+	sloth = preload("res://crates/sloth-crate.tscn"),
+	tiger = preload("res://crates/tiger-crate.tscn")
+}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,4 +19,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+#	var crateType = %GameplayLoop.targetCrateType
+#	print(crateType)
+#	texture = crateScenes[crateType]
+	
 	pass
