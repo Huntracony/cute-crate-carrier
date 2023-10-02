c2f8d55c098dbc6abc31baa19f47205b8160eeec
Huntracony <Huntracony@users.noreply.github.com>
2023-10-02 22:51:45 +0200

    post-merge fixes

--- CrateTypeImage.gd
+++ CrateTypeImage.gd
@@ -18,7 +18,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var crateType = %GameplayLoop.targetCrateType
 #	match c
 	texture = crateTextures[crateType]
